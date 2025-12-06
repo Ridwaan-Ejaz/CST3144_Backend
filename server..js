a3dc7a96a98366f16feb8ec230ba54153ae70c09
Ridwaan-Ejaz <imridwaan@gmail.com>
2025-12-06 18:26:18 +0400

    Initial backend setup with Express server and basic route, installed mongodb@3.6.3

---
+++ server..js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Backend API is running");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log("Server running on port", port);
+});
