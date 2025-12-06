9a7484cca70f1fd4aee6395e5a03ceb8a390b7c4
Ridwaan-Ejaz <imridwaan@gmail.com>
2025-12-06 18:50:48 +0400

    Connected the backend to MongoDB Atlas and set up the db reference so collections can be accessed in later routes.

--- server..js
+++ server..js
@@ -3,9 +3,29 @@ const app = express();
 
 app.use(express.json());
 
+
+const MongoClient = require('mongodb').MongoClient;
+let db;
+
+
+MongoClient.connect(
+    'mongodb+srv://rouge1:qwerty123@cluster0.tj6jqur.mongodb.net/',
+    (err, client) => {
+        if (err) {
+            console.log("MongoDB connection error:", err);
+            return;
+        }
+
+        db = client.db("AfterSchool");
+        console.log("Connected to MongoDB");
+    }
+);
+
+
 app.get('/', (req, res) => {
     res.send("Backend API is running");
 });
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
