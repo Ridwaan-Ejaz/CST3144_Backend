8e3aaa3875e4dcb29f67a67abfbdd324acee0ce2
Ridwaan-Ejaz <imridwaan@gmail.com>
2025-12-06 19:00:59 +0400

    Added CORS configuration so the frontend can communicate with the backend without blocked requests.

--- server..js
+++ server..js
@@ -3,6 +3,14 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
+    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+    res.setHeader('Access-Control-Allow-Headers', "Access-Control-Allow-Headers, Origin ,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    next();
+})
+
 
 const MongoClient = require('mongodb').MongoClient;
 let db;
